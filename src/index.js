096b03ef3f44a5ff63f9fcf164c2ca13b36483b6
Francisco Lopez <franlopbri@gmail.com>
2022-01-22 17:02:30 -0600

    Mostrando information dentro del html

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@
  **/
 
 //Web API
-const url = 'https://platzi-avo.vercel.app/api/avo';
+const baseUrl = 'https://platzi-avo.vercel.app';
+const url = `${baseUrl}/api/avo`;
 
 //Conetarse al servidor
 window
@@ -16,19 +17,22 @@ window
     const allItems = [];
     respuestaJson.data.forEach(item => {
             //Crear imagen
-            const image = document.createElement('img')
+            const image = document.createElement('img');
+            image.src = `${baseUrl}${item.image}`;
             //crear titulo
-            const title = document.createElement('h1')
+            const title = document.createElement('h1');
+            title.textContent = item.name;
             //crear precio
-            const price = document.createElement('div')
+            const price = document.createElement('div');
+            price.textContent = item.price;
             
-            const container = document.createElement('div')
+            const container = document.createElement('div');
             container.append(image, title, price);
 
             allItems.push(container);
         });
 
-        document.body.append(...allItems);
+        document.querySelector('#app').append(...allItems);
     })
 
 
