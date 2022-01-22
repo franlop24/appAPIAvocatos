6aa72991b11ab314d1f30e69ca48e7277be3b2b9
Francisco Lopez <franlopbri@gmail.com>
2022-01-22 15:31:48 -0600

    creando elementos contenedores

--- src/index.js
+++ src/index.js
@@ -3,4 +3,32 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+//Web API
+const url = 'https://platzi-avo.vercel.app/api/avo';
+
+//Conetarse al servidor
+window
+.fetch(url)
+//Procesar la respuesta y convertirla al JSON
+.then((respuesta) => respuesta.json())
+//Json -> data -> Renderizar la información en el browser
+.then(respuestaJson => {
+    const allItems = [];
+    respuestaJson.data.forEach(item => {
+            //Crear imagen
+            const image = document.createElement('img')
+            //crear titulo
+            const title = document.createElement('h1')
+            //crear precio
+            const price = document.createElement('div')
+            
+            const container = document.createElement('div')
+            container.append(image, title, price);
+
+            allItems.push(container);
+        });
+
+        document.body.append(...allItems);
+    })
+
+
